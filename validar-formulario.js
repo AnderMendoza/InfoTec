a851bcb20080d600515e1108c3fd906436281133
Angelo <73931812@certus.edu.pe>
2021-06-18 03:46:00 -0500

    Diseño del formulario terminado

--- validar-formulario.js
+++ validar-formulario.js
@@ -1,12 +1,75 @@
-var inputs = document.getElementsByClassName('login__input');
-for (var i =0; i < inputs.length; i++) {
+const slidePag = document.querySelector(".slidepage");
+const firtNextBtn = document.querySelector(".nextBtn");
 
-    inputs[i].addEventListener('keyup', function(){
-        if(this.value.length>=1) {
-            this.nextElementSibling.classlist.add('fijar');
-        } else {
-            this.nextElementSibling.classlist.remove('fijar');
-        }
-    });
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
 
-}
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let max = 4;
+let current = 1;
+//  ESTO ES DE PRUEBA //
+const nombre = document.getElementById("nombre")
+
+//
+
+firtNextBtn.addEventListener("click", function(){
+    slidePag.style.marginLeft = "-25%";
+    bullet[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    current += 1;
+});
+nextBtnSec.addEventListener("click", function(){
+    slidePag.style.marginLeft = "-50%";
+    bullet[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    current += 1;
+});
+nextBtnThird.addEventListener("click", function(){
+    slidePag.style.marginLeft = "-75%";
+    bullet[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    current += 1;
+});
+submitBtn.addEventListener("click", function(){
+    bullet[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    current += 1;
+    setTimeout(function(){
+        alert("!Tu registro ha sido exitoso!");
+        location.reload();
+    }, 800);
+});
+
+
+prevBtnSec.addEventListener("click", function(){
+    slidePag.style.marginLeft = "0%";
+    bullet[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    current -= 1;
+});
+prevBtnThird.addEventListener("click", function(){
+    slidePag.style.marginLeft = "-25%";
+    bullet[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    current -= 1;
+});
+prevBtnFourth.addEventListener("click", function(){
+    slidePag.style.marginLeft = "-50%";
+    bullet[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    current -= 1;
+});
